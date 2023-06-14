d8626ab9f9d1f620d9503f94a229321e3a29c72a
patrickbrownrigg <patrick@deposify.com>
2023-06-14 16:48:22 -0400

    Start prompt

--- src/index.ts
+++ src/index.ts
@@ -3,15 +3,54 @@ import {
   JupyterFrontEndPlugin
 } from '@jupyterlab/application';
 
+import { ICommandPalette, MainAreaWidget } from '@jupyterlab/apputils';
+
+import { Widget } from '@lumino/widgets';
+
 /**
  * Initialization data for the plotly-extension extension.
  */
 const plugin: JupyterFrontEndPlugin<void> = {
-  id: 'plotly-extension:plugin',
-  description: 'A JupyterLab extension.',
+  id: 'plotly-extension:open-dialog',
+  description: 'A dialog box with an input field.',
   autoStart: true,
-  activate: (app: JupyterFrontEnd) => {
+  requires: [ICommandPalette],
+  activate: (app: JupyterFrontEnd, palette: ICommandPalette) => {
     console.log('JupyterLab extension plotly-extension is activated!');
+
+    // Define a widget creator function,
+    // then call it to make a new widget
+    const newWidget = () => {
+      // Create a blank content widget inside of a MainAreaWidget
+      const content = new Widget();
+      const widget = new MainAreaWidget({ content });
+      widget.id = 'open-dialog';
+      widget.title.label = 'Open Dialog';
+      widget.title.closable = true;
+      return widget;
+    };
+    let widget = newWidget();
+
+    // Add an application command
+    const command = 'plotly-extension:open-dialog';
+    app.commands.addCommand(command, {
+      label: 'Dialog Prompt',
+      execute: () => {
+        // Regenerate the widget if disposed
+        if (widget.isDisposed) {
+          widget = newWidget();
+        }
+        if (!widget.isAttached) {
+          // Attach the widget to the main work area if it's not there
+          app.shell.add(widget, 'main');
+        }
+        // Activate the widget
+        app.shell.activateById(widget.id);
+      }
+    });
+
+    // Add the command to the palette.
+    palette.addItem({ command, category: 'Tutorial' });
   }
 };
 
